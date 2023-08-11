b3a48cbbb568e6bad8764316395c8684b809ec85
Maxim <38497831+StableBig@users.noreply.github.com>
2023-08-11 23:19:53 +0200

    Change title_en, title_jp models

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -6,8 +6,8 @@ class Pokemon(models.Model):
     title = models.CharField(max_length=200, verbose_name="Имя покемона")
     image = models.ImageField(upload_to="images/", null=True, blank=True, verbose_name="Изображение")
     description = models.TextField(blank=True, verbose_name="Описание")
-    title_en = models.CharField(max_length=200, blank=True, null=True, verbose_name="Имя на английском")
-    title_jp = models.CharField(max_length=200, blank=True, null=True, verbose_name="Имя на японском")
+    title_en = models.CharField(max_length=200, blank=True, verbose_name="Имя на английском")
+    title_jp = models.CharField(max_length=200, blank=True, verbose_name="Имя на японском")
     evolved_from = models.ForeignKey(
         "self",
         on_delete=models.SET_NULL,
